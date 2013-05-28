cbe4959c08c248bc568383eb87ca8124e88b43d6
David Durost <david.durost@gmail.com>
2013-05-28 09:26:54 -0400

    Updated to 2.0 of the core library

--- lib/Instagram/Api/InstagramApi.php
+++ lib/Instagram/Api/InstagramApi.php
@@ -6,15 +6,21 @@ use Instagram\Client;
 
 class InstagramApi extends AbstractApi
 {
+    /**
+     * 
+     * @var Instagram\Client $client
+     */
     protected $client = null;
     
+    /**
+     * 
+     * @param Instagram\Client $client 
+     */
     public function __construct( Client $client = null ) 
     {
         $this->client = $client instanceof Client ? $client : new Client();
 
-        $this->client->setUrl( 'https://api.instagram.com/v1/:path' );
-        $this->client->getHttpClient()->setOption( 'certificate', false ); // 'Resources/config/certificate.pem' );
-        
+        $this->client->setUrl( 'https://api.instagram.com/v1/:path' );               
     }
 
     public function getAuthClientId() {
@@ -25,7 +31,27 @@ class InstagramApi extends AbstractApi
     {
         $this->client->setAuthClientId( $id );
     }
+
+    public function getClient()
+    {
+        return $this->client;
+    }
+
+    public function setUrl( $url )
+    {
+        return $this->client->setUrl( $url );
+    }
     
+    public function setOption( $k, $v )
+    {
+        return $this->client->setOption( $k, $v );
+    }
+
+    public function get( $path, $parameters = array(), $requestOpts = array() )
+    {
+        return $this->client->get( $path, $parameters, $requestOpts );
+    }
+
     public function api( $api )
     {
         return $this->client->api( $api );
